bcf2b6cb39f57d7c4a45a037227ae9f2c21b7992
Ivony <ivony@live.com>
2018-12-11 18:24:32 +0800

    (temp)

---
+++ Ivony.Http.Pipeline.Test/RewriteTemplate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ivony.Http.Pipeline.Routes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ivony.Http.Pipeline.Test
+{
+
+  [TestClass]
+  public class RewriteTemplate
+  {
+
+    [TestMethod]
+    public void Host()
+    {
+      var template = new RewriteRequestTemplate( "//163.com/{path*}" );
+      Assert.AreEqual( template.HostTemplate.ToString(), "163.com" );
+      Assert.AreEqual( template.PathTemplate.ToString(), "{path*}" );
+    }
+
+
+    [TestMethod]
+    public void Host2()
+    {
+      var template = new RewriteRequestTemplate( "//{host}.163.com/{path*}" );
+      Assert.AreEqual( template.HostTemplate.ToString(), "{host}.163.com" );
+      Assert.AreEqual( template.PathTemplate.ToString(), "{path*}" );
+    }
+
+  }
+}
