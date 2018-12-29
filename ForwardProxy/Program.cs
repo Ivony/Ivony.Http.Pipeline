2f7df3d8627e8855a4238c8ada99baf1145cc733
Ivony <ivony@live.com>
2018-12-29 19:43:01 +0800

    v0.0.2

--- ForwardProxy/Program.cs
+++ ForwardProxy/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Ivony.Http.Pipeline.Routes;
 
 namespace ForwardProxy
 {
@@ -31,8 +32,9 @@ namespace ForwardProxy
       public void Configure( IApplicationBuilder app )
       {
         app
-          .UseForwardedHeaders()
           .ForwardProxy()
+          .UseRouter( configure => configure
+            .Rewrite( "//163.com/{path*}", "//jumony.net/{path}" ) )
           .Emit();
       }
 
